c6609899c04cd65e6d8621a48a0ac1ba19d42f36
Øyvind Hauge <oyvind.hauge@evry.com>
2019-07-14 18:27:14 +0200

    mergesort fixed

--- algorithms/sorting/mergesort.go
+++ algorithms/sorting/mergesort.go
@@ -1,41 +1,43 @@
 package sorting
 
+var aux []int
+
 // Mergesort - (NlogN running time)
 func Mergesort(a *[]int) {
-	aux := make([]int, len(*a))
-	sort(a, &aux, 0, len(*a)-1)
+	aux = make([]int, len(*a))
+	sort(a, 0, len(*a)-1)
 }
 
-func sort(a *[]int, aux *[]int, lo int, hi int) {
+func sort(a *[]int, lo int, hi int) {
 	if hi <= lo {
 		return
 	}
 	mid := lo + (hi-lo)/2
-	sort(a, aux, lo, mid)
-	sort(a, aux, mid-1, hi)
-	merge(a, aux, lo, mid, hi)
+	sort(a, lo, mid)
+	sort(a, mid+1, hi)
+	merge(a, lo, mid, hi)
 }
 
-func merge(a *[]int, aux *[]int, lo int, mid int, hi int) {
+func merge(a *[]int, lo int, mid int, hi int) {
 	i, j := lo, mid+1
 	// Copy all values from a[lo...hi] into aux[lo...hi]
-	for k := lo; k <= hi; i++ {
-		(*aux)[k] = (*a)[k]
+	for k := lo; k <= hi; k++ {
+		aux[k] = (*a)[k]
 	}
 	// Merge back to a[lo...hi]
 	for k := lo; k <= hi; k++ {
 		if i > mid {
+			(*a)[k] = aux[j]
 			j++
-			(*a)[k] = (*aux)[j]
 		} else if j > hi {
+			(*a)[k] = aux[i]
 			i++
-			(*a)[k] = (*aux)[i]
-		} else if (*aux)[j] < (*aux)[i] {
+		} else if aux[j] < aux[i] {
+			(*a)[k] = aux[j]
 			j++
-			(*a)[k] = (*aux)[j]
 		} else {
+			(*a)[k] = aux[i]
 			i++
-			(*a)[k] = (*aux)[i]
 		}
 	}
 }
--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	sorting "bitbucket.com/oyvind_hauge/inf234/algorithms/sorting"
-	"bitbucket.com/oyvind_hauge/inf234/ch1"
 )
 
 func main() {
 
-	ch1.GS
-
 	a := []int{4, 7, 2, 1, 6, 7, 1, 9, 8}
-	sorting.Mergesort(&s)
+	sorting.Mergesort(&a)
+	fmt.Printf("%v", a)
 }

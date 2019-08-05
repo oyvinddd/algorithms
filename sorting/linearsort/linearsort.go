48d5b16194d776fd291f8342093e4604cb288216
Øyvind Hauge <oyvind.s.hauge@gmail.com>
2019-08-05 22:38:52 +0200

    adding comment to linear sort

--- sorting/linearsort/linearsort.go
+++ sorting/linearsort/linearsort.go
@@ -1,7 +1,11 @@
+// Package linearsort implements the linear sort algorithm
 package linearsort
 
 /*
-Sort returns a sorted copy of the input slice
+Sort returns a sorted copy of the input slice. This algorithm
+is pretty much pointless and only works for unique, positive
+integers since it uses these values as indices when sorting.
+
 Running time: linear time proprtional to the size of the input
 */
 func Sort(a []int) []int {
@@ -10,8 +14,4 @@ func Sort(a []int) []int {
 		b[a[index]] = a[index]
 	}
 	return b
-}
-
-func SortRec() {
-
 }

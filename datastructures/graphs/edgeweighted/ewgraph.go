b18a571f5d0c61c911f489f4ce021dda630bc488
Øyvind Hauge <oyvind.hauge@evry.com>
2019-09-23 23:19:43 +0200

    ew graph

--- datastructures/graphs/edgeweighted/ewgraph.go
+++ datastructures/graphs/edgeweighted/ewgraph.go
@@ -1,5 +1,59 @@
 package graphs
 
-// EdgeWeightedGraph struct
-type EdgeWeightedGraph struct {
+import (
+	"fmt"
+
+	"github.com/oyvinddd/algorithms/datastructures/bag"
+)
+
+// EWGraph struct
+type EWGraph struct {
+	v, e int
+	adj  []bag.Bag
+}
+
+// NewEWGraph constructor
+func NewEWGraph(v int) *EWGraph {
+	g := &EWGraph{v: v, e: 0}
+	g.adj = make([]bag.Bag, v)
+	for i := 0; i < v; i++ {
+		g.adj[i] = *bag.NewBag()
+	}
+	return g
+}
+
+// NoOfV returns the number of vertices in the graph
+func (ewg EWGraph) NoOfV() int {
+	return ewg.v
+}
+
+// NoOfE returns the number of edges in the graph
+func (ewg EWGraph) NoOfE() int {
+	return ewg.e
+}
+
+// AddEdge add an edge to the graph
+func (ewg *EWGraph) AddEdge(e Edge) {
+	v := e.Either()
+	w := e.Other(v)
+	ewg.adj[v].Add(e)
+	ewg.adj[w].Add(e)
+	ewg.e++
+}
+
+// GetAdj returns an iterator over the given bag
+func (ewg EWGraph) GetAdj(v int) <-chan interface{} {
+	return ewg.adj[v].Iterator()
+}
+
+// Print graph details to the console
+func (ewg EWGraph) Print() {
+	fmt.Printf("%v vertices, %v edges\n", ewg.NoOfV(), ewg.NoOfE())
+	for v := 0; v < ewg.NoOfV(); v++ {
+		fmt.Printf("%v: ", v)
+		for w := range ewg.GetAdj(v) {
+			fmt.Printf("%v ", w)
+		}
+		fmt.Println()
+	}
 }
--- main.go
+++ main.go
@@ -1,24 +1,14 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/oyvinddd/algorithms/algorithms/greedy"
-)
+import graphs "github.com/oyvinddd/algorithms/datastructures/graphs/edgeweighted"
 
 func main() {
 
-	j1 := *greedy.NewJob("J1", 0, 2)
-	j2 := *greedy.NewJob("J2", 3, 4)
-	j3 := *greedy.NewJob("J3", 5, 6)
-	j4 := *greedy.NewJob("J4", 5, 7)
-	j5 := *greedy.NewJob("J5", 6, 11)
+	g := graphs.NewEWGraph(4)
+	g.AddEdge(*graphs.NewEdge(0, 1, 2))
+	g.AddEdge(*graphs.NewEdge(1, 2, 1))
+	g.AddEdge(*graphs.NewEdge(2, 3, 6))
+	g.AddEdge(*graphs.NewEdge(3, 0, 9))
 
-	jobs := []greedy.Job{j1, j2, j3, j4, j5}
-
-	optimalJobs := greedy.IntervalScheduling(jobs)
-
-	for _, e := range optimalJobs {
-		fmt.Printf("%v\n", e.Name)
-	}
+	g.Print()
 }

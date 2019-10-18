7aab026159bd4f8eb463dc2ad006993234f0659d
Øyvind Hauge <oyvind.hauge@evry.com>
2019-10-18 17:21:52 +0200

    tweaks to interval scheduling

--- algorithms/greedy/interval.go
+++ algorithms/greedy/interval.go
@@ -1,8 +1,6 @@
 package greedy
 
-import (
-	"sort"
-)
+import "sort"
 
 // Job struct
 type Job struct {
@@ -11,23 +9,15 @@ type Job struct {
 	End   int
 }
 
-// NewJob convenience constructor
-func NewJob(name string, start int, end int) *Job {
-	return &Job{Name: name, Start: start, End: end}
-}
-
 // IntervalScheduling returns an optimal set of compatible jobs
 // Running time: O(nlogn)
 //	- sorting
 //	- iterating n times (constant time operations each time)
 func IntervalScheduling(jobs []Job) []Job {
-	// Sort the slice according to end time O(nlogn)
-	sort.Slice(jobs, func(i, j int) bool {
-		return jobs[i].End < jobs[j].End
-	})
+	sortedJobs := jobSort(jobs)
 	optimal := make([]Job, len(jobs))
 	cnt, cnto := 0, 0
-	for cnt < len(jobs) {
+	for cnt < len(sortedJobs) {
 		job := jobs[cnt]
 		cnt++
 		if cnto > 0 && !isCompatible(optimal[cnto-1], job) {
@@ -39,6 +29,20 @@ func IntervalScheduling(jobs []Job) []Job {
 	return optimal
 }
 
+// NewJob convenience constructor
+func NewJob(name string, start int, end int) *Job {
+	return &Job{Name: name, Start: start, End: end}
+}
+
+// Private helper functions
+
 func isCompatible(this Job, that Job) bool {
 	return this.End <= that.Start
+}
+
+func jobSort(jobs []Job) []Job {
+	sort.Slice(jobs, func(i, j int) bool {
+		return jobs[i].End < jobs[j].End
+	})
+	return jobs
 }
